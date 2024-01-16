21ca9a4026c68d4121fcbe201164e312bd1cbd3f
Vagarth <vagarth419@gmail.com>
2024-01-17 00:25:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Popular Product
+    </section>
+    <section className="padding">
+      Super Quality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special Offers
+    </section>
+    <section className="padding bg-pale-blue">
+      Customer Reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      Footer
+    </section>
+  </main>
+);
+export default App;
